ab5270107cca5b4e21fadfd0c41ec31107fb8747
matsalayev <matsalayev@outlook.com>
2025-03-07 07:08:54 +0500

    fix test

--- endpoints/03-api/src/main/scala/tm/endpoint/routes/package.scala
+++ endpoints/03-api/src/main/scala/tm/endpoint/routes/package.scala
@@ -9,7 +9,7 @@ import tm.domain.enums.Role
 package object routes {
   object asAdmin {
     def unapply[F[_]](ar: AuthedRequest[F, AuthedUser]): Option[(Request[F], AuthedUser)] =
-      Option.when(ar.context.role == Role.Admin)(
+      Option.when(ar.context.role == Role.Director)(
         ar.req -> ar.context
       )
   }
